882f2d847a7d8eece8d3861d75a719e1b5976226
Anurag Singh <contact@anuragsingh.me>
2017-08-09 22:57:37 +0530

    Develop plugin

---
+++ includes/class-wordpress-plugin-template.php
@@ -0,0 +1,359 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WordPress_Plugin_Template {
+
+	/**
+	 * The single instance of WordPress_Plugin_Template.
+	 * @var 	object
+	 * @access  private
+	 * @since 	1.0.0
+	 */
+	private static $_instance = null;
+
+	/**
+	 * Settings class object
+	 * @var     object
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $settings = null;
+
+	/**
+	 * The version number.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $_version;
+
+	/**
+	 * The token.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $_token;
+
+	/**
+	 * The main plugin file.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $file;
+
+	/**
+	 * The main plugin directory.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $dir;
+
+	/**
+	 * The plugin assets directory.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $assets_dir;
+
+	/**
+	 * The plugin assets URL.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $assets_url;
+
+	/**
+	 * Suffix for Javascripts.
+	 * @var     string
+	 * @access  public
+	 * @since   1.0.0
+	 */
+	public $script_suffix;
+
+	
+	/**
+	 * Constructor function.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function __construct ( $file = '', $version = '1.0.0' ) {
+		$this->_version = $version;
+		$this->_token = 'as_remove_comments';
+
+		// Load plugin environment variables
+		$this->file = $file;
+		$this->dir = dirname( $this->file );
+		$this->assets_dir = trailingslashit( $this->dir ) . 'assets';
+		$this->assets_url = esc_url( trailingslashit( plugins_url( '/assets/', $this->file ) ) );
+
+		$this->script_suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		register_activation_hook( $this->file, array( $this, 'install' ) );
+
+		// Load frontend JS & CSS
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ), 10 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 10 );
+
+		// Load admin JS & CSS
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ), 10, 1 );
+		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_styles' ), 10, 1 );
+
+		// Load API for generic admin functions
+		if ( is_admin() ) {
+			$this->admin = new WordPress_Plugin_Template_Admin_API();
+		}
+
+		// Handle localisation
+		$this->load_plugin_textdomain();
+		add_action( 'init', array( $this, 'load_localisation' ), 0 );
+
+	} // End __construct ()
+
+	/**
+	 * Wrapper function to register a new post type
+	 * @param  string $post_type   Post type name
+	 * @param  string $plural      Post type item plural name
+	 * @param  string $single      Post type item single name
+	 * @param  string $description Description of post type
+	 * @return object              Post type class object
+	 */
+	public function register_post_type ( $post_type = '', $plural = '', $single = '', $description = '', $options = array() ) {
+
+		if ( ! $post_type || ! $plural || ! $single ) return;
+
+		$post_type = new WordPress_Plugin_Template_Post_Type( $post_type, $plural, $single, $description, $options );
+
+		return $post_type;
+	}
+
+	/**
+	 * Wrapper function to register a new taxonomy
+	 * @param  string $taxonomy   Taxonomy name
+	 * @param  string $plural     Taxonomy single name
+	 * @param  string $single     Taxonomy plural name
+	 * @param  array  $post_types Post types to which this taxonomy applies
+	 * @return object             Taxonomy class object
+	 */
+	public function register_taxonomy ( $taxonomy = '', $plural = '', $single = '', $post_types = array(), $taxonomy_args = array() ) {
+
+		if ( ! $taxonomy || ! $plural || ! $single ) return;
+
+		$taxonomy = new WordPress_Plugin_Template_Taxonomy( $taxonomy, $plural, $single, $post_types, $taxonomy_args );
+
+		return $taxonomy;
+	}
+
+	/**
+	 * Load frontend CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return void
+	 */
+	public function enqueue_styles () {
+		wp_register_style( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'css/frontend.css', array(), $this->_version );
+		wp_enqueue_style( $this->_token . '-frontend' );
+	} // End enqueue_styles ()
+
+	/**
+	 * Load frontend Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function enqueue_scripts () {
+		wp_register_script( $this->_token . '-frontend', esc_url( $this->assets_url ) . 'js/frontend' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		wp_enqueue_script( $this->_token . '-frontend' );
+	} // End enqueue_scripts ()
+
+	/**
+	 * Load admin CSS.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_styles ( $hook = '' ) {
+		wp_register_style( $this->_token . '-admin', esc_url( $this->assets_url ) . 'css/admin.css', array(), $this->_version );
+		wp_enqueue_style( $this->_token . '-admin' );
+	} // End admin_enqueue_styles ()
+
+	/**
+	 * Load admin Javascript.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function admin_enqueue_scripts ( $hook = '' ) {
+		wp_register_script( $this->_token . '-admin', esc_url( $this->assets_url ) . 'js/admin' . $this->script_suffix . '.js', array( 'jquery' ), $this->_version );
+		wp_enqueue_script( $this->_token . '-admin' );
+	} // End admin_enqueue_scripts ()
+
+	/**
+	 * Load plugin localisation
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_localisation () {
+		load_plugin_textdomain( 'wordpress-plugin-template', false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_localisation ()
+
+	/**
+	 * Load plugin textdomain
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function load_plugin_textdomain () {
+	    $domain = 'wordpress-plugin-template';
+
+	    $locale = apply_filters( 'plugin_locale', get_locale(), $domain );
+
+	    load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
+	    load_plugin_textdomain( $domain, false, dirname( plugin_basename( $this->file ) ) . '/lang/' );
+	} // End load_plugin_textdomain ()
+
+	/**
+	 * Main WordPress_Plugin_Template Instance
+	 *
+	 * Ensures only one instance of WordPress_Plugin_Template is loaded or can be loaded.
+	 *
+	 * @since 1.0.0
+	 * @static
+	 * @see WordPress_Plugin_Template()
+	 * @return Main WordPress_Plugin_Template instance
+	 */
+	public static function instance ( $file = '', $version = '1.0.0' ) {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self( $file, $version );
+		}
+		return self::$_instance;
+	} // End instance ()
+
+	/**
+	 * Cloning is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __clone () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __clone ()
+
+	/**
+	 * Unserializing instances of this class is forbidden.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __wakeup () {
+		_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?' ), $this->_version );
+	} // End __wakeup ()
+
+	/**
+	 * Installation. Runs on activation.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	public function install () {
+		$this->_log_version_number();
+	} // End install ()
+
+	/**
+	 * Log the plugin version number.
+	 * @access  public
+	 * @since   1.0.0
+	 * @return  void
+	 */
+	private function _log_version_number () {
+		update_option( $this->_token . '_version', $this->_version );
+	} // End _log_version_number ()
+
+	public function get_all_selected_posts () {
+		$post_types = get_option('as_hide_comments_for_post_types');
+		return $post_types;
+	}
+
+	public function as_hide_comments() {
+		add_action('admin_init', 'df_disable_comments_post_types_support');
+
+		$post_types = $this->get_all_selected_posts();
+
+		if(in_array('all', $post_types)){
+			add_filter('comments_open', 'df_disable_comments_status', 20, 2);
+			add_filter('pings_open', 'df_disable_comments_status', 20, 2);
+			add_filter('comments_array', 'df_disable_comments_hide_existing_comments', 10, 2);
+			add_action('admin_menu', 'df_disable_comments_admin_menu');
+			add_action('admin_init', 'df_disable_comments_admin_menu_redirect');
+			add_action('admin_init', 'df_disable_comments_dashboard');
+			add_action('init', 'df_disable_comments_admin_bar');
+			add_action( 'wp_before_admin_bar_render', 'my_admin_bar_render' );
+		}
+
+		
+	}
+
+
+}
+
+
+
+	// Disable support for comments and trackbacks in post types
+	function df_disable_comments_post_types_support() {
+		//$post_types = get_post_types();
+
+		$post_types = get_option('as_hide_comments_for_post_types');
+
+		foreach ($post_types as $post_type) {
+			if(post_type_supports($post_type, 'comments')) {
+				remove_post_type_support($post_type, 'comments');
+				remove_post_type_support($post_type, 'trackbacks');
+			}
+		}
+	}
+
+	// Close comments on the front-end
+	function df_disable_comments_status() {
+		return false;
+	}
+
+	// Hide existing comments
+	function df_disable_comments_hide_existing_comments($comments) {
+		$comments = array();
+		return $comments;
+	}
+
+	// Remove comments page in menu
+	function df_disable_comments_admin_menu() {
+		remove_menu_page('edit-comments.php');
+	}
+
+	// Redirect any user trying to access comments page
+	function df_disable_comments_admin_menu_redirect() {
+		global $pagenow;
+		if ($pagenow === 'edit-comments.php') {
+			wp_redirect(admin_url()); exit;
+		}
+	}
+
+	// Remove comments metabox from dashboard
+	function df_disable_comments_dashboard() {
+		remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+	}
+
+	// Remove comments links from admin bar
+	function df_disable_comments_admin_bar() {
+			if (is_admin_bar_showing()) {
+					remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+			}
+	}
+
+	function my_admin_bar_render() {
+		global $wp_admin_bar;
+		$wp_admin_bar->remove_menu('comments');
+	}
